9fe80570495bfa42c506fbc2feb8f925c5998151
BaoJin <130841308@qq.com>
2020-02-13 16:53:43 +0800

    更新

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,18 +1,52 @@
 import React, {Component} from 'react'
+import {Form, Icon, Input, Button} from 'antd';
+import axios from 'axios'
 import logo from './images/logo.png'
 import './css/login.less'
-import {Form, Icon, Input, Button} from 'antd';
 
 class Login extends Component {
+
+    validatorPassword = (rule, value, callback) => {
+        if (!value) {
+            callback('请输入密码')
+        }else if (value.length > 12) {
+            callback('密码必须小于12位')
+        }else if (value.length < 4) {
+            callback('密码必须大于4位')
+        }else if (!(/^\w+$/)) {
+            callback('密码必须是英文、数字或下划线组成')
+        }else{
+            callback()
+        }
+    }
+    
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                const {username,password} = values
+                // console.log('Received values of form: ', values);
+                
+                axios.post('http://localhost:3000/login',`username=${username}&password=${password}`).then(
+                    (response) => {console.log(response.data);},
+                    (error) => {console.log(err);}
+                   
+                )
+                
+            }
+        });
+
+    }
+    
+
     render() {
 
         const { getFieldDecorator } = this.props.form;
-
         return (
             <div id="login">
                 <div className="header">
                     <img src={logo} alt="logo"/>
-                    <h1>商品管理系统</h1>
+                    <h1>佩奇网上商城管理系统</h1>
                 </div>
                 <div className="content">
                     <h2>用户登录</h2>
@@ -21,9 +55,9 @@ class Login extends Component {
                         <Form.Item>
                             {getFieldDecorator('username', {
                                rules: [
-                                {required: true, message: '你让我猜你叫啥啊?'},
-                                {max:8,message: '别tm输太多你是不傻'},
-                                {min:2,message: '你起名就一个字阿?'},
+                                {required: true, message: '请输入用户名!'},
+                                {max:8,message: '用户名必须小于8位!'},
+                                {min:2,message: '用户名必须大于2位'},
                                 {pattern:/^\w+$/,message: '用户名必须是英文、数字或下划线组成'}
                                 ],
                             })(
@@ -36,10 +70,7 @@ class Login extends Component {
                         <Form.Item>
                             {getFieldDecorator('password', {
                                 rules: [
-                                    { required: true, message: '我给你记密码阿?' },
-                                    {pattern:/^\w+$/,message: '别逼我骂你啊'}
-                                    
-
+                                   {validator:this.validatorPassword}
                                 ],
                             })(
                                 <Input
